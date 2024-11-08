801d5d987c71d08f2867e4516e869b282f946c07
anargh-t <anarght@gmail.com>
2024-11-08 12:38:10 +0530

    add deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox")
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: {
-      version: "0.8.20",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 1000,
-        },
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      // See its defaults
+    }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
       },
     },
-  };
+  },
+};
